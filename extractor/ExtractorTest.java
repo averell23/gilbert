ebc893a1e1b378b11a22e240237884e775eca57e
dhahn <>
2002-03-12 15:16:29 +0000

    Extractor can be selected without recompiling.

--- extractor/ExtractorTest.java
+++ extractor/ExtractorTest.java
@@ -13,30 +13,50 @@ import org.apache.log4j.xml.*;
 /**
  *
  * @author  daniel
- * @version 
+ * @version
  */
 public class ExtractorTest {
-
+    
     /**
-    * @param args the command line arguments
-    */
-    public static void main (String args[]) {
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
         Logger mainlogger = Logger.getLogger("main");
         PropertyConfigurator.configure("gilbert/extractor/log4j.properties");
-        
         if (args.length == 0) {
-            System.err.println("Please give the uri to open.");
+            System.err.println("Usage: [extractor] <source uri>");
             System.exit(1);
         }
+        String extractor = "DumpExtractor";
+        String uri = "";
+        if (args.length > 1) {
+            extractor = args[0];
+            uri = args[1];
+        } else {
+            uri = args[0];
+        }
+        
         mainlogger.info("Starting...");
-        Extractor x = new StraightExtractor();
-        x.addPrefilter(new LocalVisitFilter());
+        Class extClass = null;
+        Extractor x = null;
         long timestamp = System.currentTimeMillis();
-        x.extract(args[0]);
-        x.getOutputStream().flush();
+        try {
+            extClass = Class.forName("gilbert.extractor.extractors." + extractor);
+            x = (Extractor) extClass.newInstance();
+            x.addPrefilter(new LocalVisitFilter());
+            timestamp = System.currentTimeMillis();
+            x.extract(uri);
+            x.getOutputStream().flush();
+        } catch (ClassNotFoundException e) {
+            mainlogger.error("Cannot find class: " + e.getMessage(), e);
+        } catch (InstantiationException e) {
+            mainlogger.error("Cannot instantiate class: " + e.getMessage(), e);
+        } catch (IllegalAccessException e) {
+            mainlogger.error("Access violation: " + e.getMessage(), e);
+        }
         long time = (System.currentTimeMillis() - timestamp) / 1000;
         mainlogger.info("Extracting time was " + time + " seconds.");
         // System.out.println(Util.liveCache);
     }
-
+    
 }
